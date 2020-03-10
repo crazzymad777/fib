02de69c6fbfa518ff5b5a0dce6bd8ceb989d9547
Yuri Moskov <aerobus.778@gmail.com>
2020-03-11 02:38:30 +0300

    fix database.js

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize');
 
-require('custom-env').env(".env");
+require('custom-env').env('.env');
 
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-  dialect: 'mysql',
-  host: process.env.MYSQL_HOST
-})
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.MYSQL_HOST,
+  });
 
 module.exports = sequelize;
