c77b1fde4fbe9245cd6f7e6edc7323b86e38c71b
Yuri Moskov <aerobus.778@gmail.com>
2020-03-11 02:35:44 +0300

    fix app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let historyRouter = require('./routes/history');
-let calculateRouter = require('./routes/calculate');
+const indexRouter = require('./routes/index');
+const historyRouter = require('./routes/history');
+const calculateRouter = require('./routes/calculate');
 
-let app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,12 +25,12 @@ app.use('/history', historyRouter);
 app.use('/calculate', calculateRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
