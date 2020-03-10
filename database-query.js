b88225709222228439b7929508ef96367c381a91
Yuri Moskov <aerobus.778@gmail.com>
2020-03-11 02:41:08 +0300

    fix database-query.js

--- database-query.js
+++ database-query.js
@@ -1,15 +1,17 @@
 const Sequelize = require('sequelize');
-let sequelize = require('./database');
+const sequelize = require('./database');
 
-const Model = Sequelize.Model;
+const { Model } = Sequelize;
 class Query extends Model {}
 
 Query.init({
   ip: Sequelize.STRING(39),
   timestamp: Sequelize.DATE,
   source: Sequelize.INTEGER,
-  result: Sequelize.INTEGER
-}, { timestamps: false, sequelize, modelName: 'Query', tableName: 'queries'});
+  result: Sequelize.INTEGER,
+}, {
+  timestamps: false, sequelize, modelName: 'Query', tableName: 'queries',
+});
 Query.removeAttribute('id');
 
 module.exports = Query;
