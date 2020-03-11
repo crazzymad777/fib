a1bfdb3bc6a5426f89b212b099c75d40dae8ff62
Yuri Moskov <aerobus.778@gmail.com>
2020-03-11 03:09:46 +0300

    some fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,42 @@
 import React, { Component } from 'react';
-import Query from './Query';
-
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
-} from "react-router-dom";
+  Link,
+} from 'react-router-dom';
+import Query from './Query';
+
 import './App.css';
 
 class App extends Component {
-  state = {
-    number: '',
-    result: '',
-    response: '',
-    responseToPost: ''
-  };
-  
+  constructor() {
+    super();
+    this.state = {
+      number: '',
+      response: '',
+      responseToPost: '',
+    };
+  }
+
+
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res }))
-      .catch(err => console.log(err));
+      .then((res) => this.setState({ response: res }))
+      .catch((err) => console.error(err));
   }
-  
+
   callApi = async () => {
     const response = await fetch('/history');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
 
-    const items = body.response.map((query) => <Query query={query}/>);
+    const items = body.response.map((query) => <Query query={query} />);
 
     return items;
   };
-  
-  handleSubmit = async e => {
+
+  handleSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch('/calculate', {
       method: 'POST',
@@ -43,51 +46,54 @@ class App extends Component {
       body: JSON.stringify({ number: this.state.number }),
     });
     const body = await response.json();
-    
+
     this.setState({ responseToPost: body.result });
   };
-  
-render() {
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <Router>
-          <nav>
-          <ul>
-            <li>
-              <Link to="/calculate" className="App-link">Calculate</Link>
-            </li>
-            <li>
-              <Link to="/history" className="App-link">History</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/calculate">
-            <form onSubmit={this.handleSubmit}>
-              <p>
-                <strong>Index number:</strong>
-              </p>
-              <input
-                type="number"
-                value={this.state.number}
-                onChange={e => this.setState({ number: e.target.value })}
-                placeholder="enter index number"
-              />
-              <button type="submit">Calculate</button>
-            </form>
-            <p>Output: {this.state.responseToPost}</p>
-          </Route>
-          <Route path="/history">
-            <p>
-              <strong>History of Queries</strong>
-            </p>
-            <div class="wrapper">
-            {this.state.response}
-            </div>
-          </Route>
-        </Switch>
-        </Router>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/calculate" className="App-link">Calculate</Link>
+                </li>
+                <li>
+                  <Link to="/history" className="App-link">History</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/calculate">
+                <form onSubmit={this.handleSubmit}>
+                  <p>
+                    <strong>Index number:</strong>
+                  </p>
+                  <input
+                    type="number"
+                    value={this.state.number}
+                    onChange={(e) => this.setState({ number: e.target.value })}
+                    placeholder="enter index number"
+                  />
+                  <button type="submit">Calculate</button>
+                </form>
+                <p>
+Output:
+                  {this.state.responseToPost}
+                </p>
+              </Route>
+              <Route path="/history">
+                <p>
+                  <strong>History of Queries</strong>
+                </p>
+                <div className="wrapper">
+                  {this.state.response}
+                </div>
+              </Route>
+            </Switch>
+          </Router>
         </header>
       </div>
     );
--- frontend/src/Query.js
+++ frontend/src/Query.js
@@ -1,16 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Query extends Component {
-    render() {
-        return (
-            <div class="query">
-                <p class="alpha">IP: {this.props.query.ip}</p>
-                <p class="beta">Timestamp: {this.props.query.timestamp}</p>
-                <p class="gamma">Index: {this.props.query.source}</p>
-                <p class="delta">Result: {this.props.query.result}</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="query">
+        <p className="alpha">
+IP:
+          {this.props.query.ip}
+        </p>
+        <p className="beta">
+Timestamp:
+          {this.props.query.timestamp}
+        </p>
+        <p className="gamma">
+Index:
+          {this.props.query.source}
+        </p>
+        <p className="delta">
+Result:
+          {this.props.query.result}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Query;
