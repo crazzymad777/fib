e4953ed940d1fe212d70c859869614d983212202
Юрий Москов <aerobus.778@gmail.com>
2020-03-12 23:45:19 +0300

    fix Code of Style

--- frontend/src/Query.js
+++ frontend/src/Query.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Query extends Component {
-  render() {
-    return (
-      <div className="query">
-        <p className="alpha">
-IP:
-          {this.props.query.ip}
-        </p>
-        <p className="beta">
-Timestamp:
-          {this.props.query.timestamp}
-        </p>
-        <p className="gamma">
-Index:
-          {this.props.query.source}
-        </p>
-        <p className="delta">
-Result:
-          {this.props.query.result}
-        </p>
-      </div>
-    );
-  }
-}
+const Query = ({
+  ip, timestamp, source, result,
+}) => (
+  <div className="query">
+    <p className="alpha">
+            IP:
+      {ip}
+    </p>
+    <p className="beta">
+            Timestamp:
+      {timestamp}
+    </p>
+    <p className="gamma">
+            Index:
+      {source}
+    </p>
+    <p className="delta">
+            Result:
+      {result}
+    </p>
+  </div>
+);
+
+
+Query.propTypes = {
+  ip: PropTypes.string,
+  timestamp: PropTypes.string,
+  source: PropTypes.number,
+  result: PropTypes.number,
+};
+
+Query.defaultProps = {
+  ip: '::1',
+  timestamp: '1970-01-01',
+  source: 1,
+  result: 1,
+};
 
 export default Query;
