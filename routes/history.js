a84d2e2309b285e138816134fba4c6309e9c66b6
Юрий Москов <aerobus.778@gmail.com>
2020-03-15 17:04:14 +0300

    validate offset and limit

--- routes/history.js
+++ routes/history.js
@@ -4,12 +4,23 @@ const router = express.Router();
 
 const Query = require('../database-query');
 
-const { sendResponse } = require('../helpful-functions');
+const { sendResponse, isInteger } = require('../helpful-functions');
 
 router.get('/', (req, res) => {
-  // validate offset and limit
   const offset = Number(req.query.offset || req.body.offset || 0);
   const limit = Number(req.query.limit || req.body.limit || 10);
+
+  // offset should be positive integer
+  if (offset <= 0 || !isInteger(offset)) {
+    sendResponse(res, { error: { message: 'offset is not positive integer.' } });
+    return;
+  }
+
+  // limit should be positive integer
+  if (limit <= 0 || !isInteger(limit)) {
+    sendResponse(res, { error: { message: 'limit is not positive integer.' } });
+    return;
+  }
 
   Query.findAndCountAll({ offset, limit }).then((queries) => {
     sendResponse(res, { response: queries });
