ad24d1f4edc261b764f30fbebd20989995bb63a4
Юрий Москов <aerobus.778@gmail.com>
2020-03-15 17:00:32 +0300

    createError, next unused add comments

--- routes/calculate.js
+++ routes/calculate.js
@@ -1,4 +1,3 @@
-const createError = require('http-errors');
 const express = require('express');
 
 const router = express.Router();
@@ -7,34 +6,40 @@ const Query = require('../database-query');
 
 const { sendResponse, isInteger, fibonacci } = require('../helpful-functions');
 
-router.all('/', (req, res, next) => {
+router.all('/', (req, res) => {
   const number = req.query.number || req.body.number;
 
+  // is number defined?
   if (number === undefined) {
-    sendResponse(res, { error: { message: 'missing number argument.' } });
+    sendResponse(res, { error: { message: 'Missing number argument.' } });
     return;
   }
 
+  // number should be positive integer
   if (number <= 0 || !isInteger(number)) {
-    sendResponse(res, { error: { message: 'number is not positive integer.' } });
+    sendResponse(res, { error: { message: 'Number is not positive integer.' } });
     return;
   }
 
   let result = 0;
   try {
+    // calculate fibonacci number
     result = fibonacci(number);
     if (result === Infinity) {
-      sendResponse(res, { error: { message: 'result too big' } });
+      sendResponse(res, { error: { message: 'Result too big' } });
       return;
     }
   } catch (e) {
     if (e instanceof RangeError) {
-      sendResponse(res, { error: { message: 'result too big' } });
+      // Probably maximum call stack size exceeded.
+      sendResponse(res, { error: { message: 'Result too big' } });
       return;
     }
+    // Unknown exception
     throw e;
   }
 
+  // save result
   Query.create({ ip: req.connection.remoteAddress, source: number, result });
 
   sendResponse(res, { result });
